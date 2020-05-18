e30c4121023c848b308fd44ebc290f748618833e
Ben Alman <cowboy@rj3.net>
2020-05-17 23:00:07 -0400

    Attempt to get release action to work

--- .github/build.sh
+++ .github/build.sh
@@ -5,7 +5,7 @@ set -o pipefail
 set -o nounset
 # set -o xtrace
 
-RELEASE_ID=$(jq --raw-output '.release.id' "$GITHUB_EVENT_PATH")
+VERSION=$(jq --raw-output '.release.tag_name' "$GITHUB_EVENT_PATH")
 
 mkdir build
-sed "s/(in-development)/$RELEASE_ID/" update-plex.sh > build/update-plex.sh
+sed "s/(in-development)/$VERSION/" update-plex.sh > build/update-plex.sh
